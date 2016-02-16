4e8261e09b8c6fdbb823b043bdd560b96afe21fc
Sumedha Kucherlapati <kucherla@usc.edu>
2016-02-15 18:42:41 -0800

    Batch and online algorithms

---
+++ LD_BatchAlgo.m
@@ -0,0 +1,34 @@
+% Batch learning algorithm using gradient discent for Linear Discrimination
+function[Jmin,theta_N,J] = LD_BatchAlgo(filename,lr,MAX_ITR)
+[output] = readtable(filename);
+    features = length(output.Properties.VariableNames)-1;
+    [input] = output(:,1:features);
+    [class] = output(:,length(output.Properties.VariableNames));
+
+    %converting table to matrix form
+    x = cell2mat(table2cell(input));
+    y = cell2mat(table2cell(class));
+    m = length(y);
+    
+   a = -0.01;
+   b = 0.01;
+    theta = (b-a).*rand(features,1) + a;
+    J = zeros(MAX_ITR, 1); 
+    teta = zeros(MAX_ITR, 3);
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    for k = 1:MAX_ITR
+        % linear combination and sigmoid function calculation
+        h = (1+exp((x * theta).*-1)).^(-1);
+        
+        % Error calculation
+        J(k,1) = -sum((y.*log(h))+((1-y).*log(1-h)))/m; 
+        
+        %Gradient calculation
+        grad = ( x' * (y-h))/m;
+        theta = theta + lr .* grad;
+        teta(k,:) = theta';
+    end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Jmin = J(find(J==min(J)));
+theta_N = teta(find(J==min(J)),:);
+end
---
+++ LD_OnlineAlg.m
@@ -0,0 +1,44 @@
+% Online learning algorithm using gradient discent for Linear Discrimination
+function[Efnmin,wt_min,Efn] = LD_OnlineAlg(filename,lr,Max_RUN)
+[output] = readtable(filename);
+features = length(output.Properties.VariableNames)-1;
+[input] = output(:,1:features);
+[class] = output(:,length(output.Properties.VariableNames));
+
+%converting table to matrix form
+x = cell2mat(table2cell(input)); % input
+r = cell2mat(table2cell(class)); % output
+m = length(r); % size of dataset
+
+% learning rate
+	lr = lr;
+   a = -0.01;
+   b = 0.01;
+    weight = (b-a).*rand(features,1) + a;
+    Efn = zeros(Max_RUN, 1); 
+    wt = zeros(Max_RUN, 3);
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        for itr = 1:Max_RUN
+            effn = 0;
+            for p = 1:m
+                % linear combination
+                lrcom = (x(p,:) * weight);
+                lrcom = -1 .* lrcom; 
+                % Calculate sigmoid and Efn term
+                y = 1/(1+exp(lrcom(1)));
+                effn = effn +(r(p,:)*log(y))+((1-r(p,:))*log(1-y)); 
+                % gradient calculation
+                tx = x'; %transpose(x);
+                grad =  tx(:,p) * ((r(p,:) - y)); 
+
+                % weight update
+                weight = weight + lr* grad;
+                wt(itr,:) = transpose(weight);
+            end
+            Efn(itr,1) = - effn / 100 ;
+        end
+     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    Efnmin = Efn(find(Efn==min(Efn)));
+    wt_min = wt(find(Efn==min(Efn)),:);
+end
+
